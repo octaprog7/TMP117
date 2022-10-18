390ca8732e86eed9c627e988caf9619c15d60146
octaprog7 <goctaprog@gmail.com>
2022-10-18 23:19:50 +0500

    + _get_flags and is_data_ready methods

--- tmp117ti.py
+++ tmp117ti.py
@@ -3,37 +3,10 @@
 # Copyright (c) 2022 Roman Shevchik   goctaprog@gmail.com
 import micropython
 from sensor_pack import bus_service
-from sensor_pack.base_sensor import BaseSensor, Iterator
+from sensor_pack.base_sensor import BaseSensor, Iterator, check_value
 
 
 # Please read this before use!: https://www.ti.com/product/TMP117
-
-
-@micropython.native
-def _check_value(value: int, valid_range, error_msg: str) -> int:
-    if value not in valid_range:
-        raise ValueError(error_msg)
-    return value
-
-
-@micropython.native
-def get_conversion_cycle_time(conversion_cycle_time: int, average: int) -> int:
-    """возвращает время преобразования температуры датчиком
-    в зависимости от его настроек"""
-    _ = _check_value(conversion_cycle_time, range(0, 8),
-                     f"Invalid conversion cycle time value: {conversion_cycle_time}")
-    _ = _check_value(average, range(0, 4),
-                     f"Invalid conversion averaging mode value: {average}")
-    avg_0 = 16, 125, 250, 500, 1000, 4000, 8000, 16000  # in [ms]
-    if average < 2:
-        if 0 == conversion_cycle_time and 1 == average:
-            return 125
-        return avg_0[conversion_cycle_time]
-    # average >= 2
-    if conversion_cycle_time < 4:
-        return 500*(average - 2)
-    # conversion_cycle_time >= 4
-    return avg_0[conversion_cycle_time]
 
 
 class TMP117(BaseSensor, Iterator):
@@ -48,6 +21,10 @@ class TMP117(BaseSensor, Iterator):
             10: Continuous conversion (CC), Same as 00
             11: One-shot conversion (OS)
 
+        conversion_cycle_time   0..7:
+            время ожидания между преобразованиями.
+            See Table 7-7 for the standby time between conversions.
+
         average (Conversion averaging modes):
             00: No averaging
             01: 8 Averaged conversions
@@ -55,14 +32,47 @@ class TMP117(BaseSensor, Iterator):
             11: 64 averaged conversions
             """
         super().__init__(adapter, address, True)
-        self.conversion_mode = _check_value(conversion_mode, range(0, 4),
-                                            f"Invalid conversion_mode value: {conversion_mode}")
-        self.conversion_cycle_time = _check_value(conversion_cycle_time, range(0, 8),
-                                                  f"Invalid conversion_cycle_time value: {conversion_cycle_time}")
-        self.average = _check_value(average, range(0, 4), f"Invalid conversion_mode value: {average}")
+        self.conversion_mode = check_value(conversion_mode, range(0, 4),
+                                           f"Invalid conversion_mode value: {conversion_mode}")
+        self.conversion_cycle_time = check_value(conversion_cycle_time, range(0, 8),
+                                                 f"Invalid conversion_cycle_time value: {conversion_cycle_time}")
+        self.average = check_value(average, range(0, 4), f"Invalid conversion_mode value: {average}")
         self.DR_Alert = self.POL = self.T_nA = False
         self.data_ready = self.low_alert = self.high_alert = False
         #
+        self.set_config()
+
+    @micropython.native
+    def get_conversion_cycle_time(self) -> int:
+        """возвращает время преобразования температуры датчиком
+        в зависимости от его настроек"""
+        _ = check_value(self.conversion_cycle_time, range(0, 8),
+                        f"Invalid conversion cycle time value: {self.conversion_cycle_time}")
+        _ = check_value(self.average, range(0, 4),
+                        f"Invalid conversion averaging mode value: {self.average}")
+        avg_0 = 16, 125, 250, 500, 1000, 4000, 8000, 16000  # in [ms]
+        if 0x03 == self.conversion_mode:    # One-shot conversion mode
+            # Когда биты MOD[1:0] в регистре конфигурации установлены на 11, TMP117 будет выполнять преобразование
+            # температуры, называемое однократным преобразованием. После того, как устройство завершит однократное
+            # преобразование, оно переходит в режим отключения с низким энергопотреблением. Однократный цикл
+            # преобразования, в отличие от непрерывного режима преобразования, состоит только из активного времени
+            # преобразования и не имеет периода ожидания. Таким образом, продолжительность однократного преобразования
+            # зависит только от битовых настроек AVG. Биты CONV не влияют на продолжительность
+            # однократного преобразования.
+            s_shot = 16, 125, 500, 1000
+            return s_shot[self.average]
+        if self.average < 2:
+            if 0 == self.conversion_cycle_time and 1 == self.average:
+                return 125
+            return avg_0[self.conversion_cycle_time]
+        # average >= 2
+        if self.conversion_cycle_time < 4:
+            return 500 * (self.average - 2)
+        # conversion_cycle_time >= 4
+        return avg_0[self.conversion_cycle_time]
+
+    def __del__(self):
+        self.conversion_mode = 0x01     # Shutdown (SD)
         self.set_config()
 
     def _read_register(self, reg_addr, bytes_count=2) -> bytes:
@@ -79,7 +89,8 @@ class TMP117(BaseSensor, Iterator):
     def _get_config_reg(self) -> int:
         """read config from register (2 byte)"""
         reg_val = self._read_register(0x01, 2)
-        return self.unpack("H", reg_val)[0]
+        config = self.unpack("H", reg_val)[0]
+        return config
 
     def _set_config_reg(self, cfg: int) -> int:
         """write config to register (2 byte)"""
@@ -137,8 +148,24 @@ class TMP117(BaseSensor, Iterator):
         res = self._read_register(0x0F, 2)
         return self.unpack("H", res)[0]
 
+    def _get_flags(self) -> tuple:
+        """Return tuple: (EEPROM_Busy, Data_Ready, LOW_Alert) flags"""
+        config = self._get_config_reg()
+        # print(f"config_reg: {hex(config)}")
+        return tuple([0 != (config & (0x01 << i)) for i in range(12, 16)])
+
+    def is_data_ready(self) -> bool:
+        """Флаг готовности данных. Этот флаг указывает, что преобразование завершено и регистр температуры
+        может быть прочитан. Каждый раз, когда считывается регистр температуры или регистр конфигурации,
+        этот бит сбрасывается!
+        Data ready flag.
+        This flag indicates that the conversion is complete and the
+        temperature register can be read. Every time the temperature
+        register or configuration register is read, this bit is cleared."""
+        return self._get_flags()[1]
+
     @micropython.native
-    def get_temperature(self):
+    def get_temperature(self) -> float:
         """return temperature most recent conversion"""
         reg_val = self._read_register(0x00, 2)
         return TMP117.__scale * self.unpack("h", reg_val)[0]
